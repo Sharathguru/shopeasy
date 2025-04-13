a60f55402404a7501e040df25bf28e369947d4c4
Sharathguru <vsarath200219@gmail.com>
2025-04-13 17:32:02 +0530

    App.js Modified :Mesage is Changed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT =process.env.PORT || 3000;
 
 
 app.get("/",(req,res,next)=>{
-    res.send("welcome to shopEasy")
+    res.send("welcome to shopEasy from JVLcode!")
 })
 
 app.listen(PORT,()=>{
