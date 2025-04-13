4de7ee3658b393a856294ed65cd697acf9fad987
Sharathguru <vsarath200219@gmail.com>
2025-04-13 23:19:38 +0530

    App.js Change in Message

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT =process.env.PORT || 3000;
 
 
 app.get("/",(req,res,next)=>{
-    res.send("welcome to shopEasy from JVLco!")
+    res.send("welcome to shopEasy from JVLcode!")
 })
 
 let server=app.listen(PORT,()=>{
